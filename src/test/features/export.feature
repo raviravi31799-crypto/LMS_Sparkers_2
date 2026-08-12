c90065fb40894061146b7b8b447cd9964abf0e93
Harini R G <harinirg26@gmail.com>
2026-08-13 00:36:44 +0530

    reused create training flow for delete scenario

--- src/test/features/export.feature
+++ src/test/features/export.feature
@@ -2,7 +2,7 @@
 Feature: Export Employee Data to Excel
 
   Background:
-      Given the user navigates to the Employee Training Records page
+    Given the user navigates to the Employee Training Records page
   Scenario: Verify Export to Excel functionality 
     When the user clicks the Export to Excel button
     Then the Excel file should be downloaded successfully
--- src/test/steps/deletestep.ts
+++ src/test/steps/deletestep.ts
@@ -1,11 +1,41 @@
-import { Given,When, Then } from "@cucumber/cucumber";
+import { When, Then } from "@cucumber/cucumber";
+import fs from "fs";
+import path from "path";
 import { CustomWorld } from "../world/world";
-import deleteData from "../../../testdata/deleteData.json";
+import { CreateTrainingPage } from "../pages/createTrainingPage";
 
-When('the user clicks the Delete icon', async function (this: CustomWorld) {
-    await this.deletepage.deleteEmployee(deleteData.empId);
+let createdEmpId: string;
+
+When("the user clicks the Delete icon", async function (this: CustomWorld) {
+
+    const createTrainingPage = new CreateTrainingPage(this.page!);
+    const filePath = path.join(process.cwd(),"testdata","deleteData.json");
+
+    const raw = JSON.parse(fs.readFileSync(filePath, "utf-8"));
+    const ts = Date.now();
+    const record = {
+        projectName: raw.projectName,
+        empId: `${raw.empId}_${ts}`,
+        employeeName: raw.employeeName,
+        course: raw.course,
+        trainerName: raw.trainerName,
+        trainingType: raw.trainingType,
+        startDate: raw.today,
+        endDate: raw.endDate,
+        status: raw.status,
+        percentCompleted: raw.percentCompleted,
+    };
+    createdEmpId = record.empId;
+    console.log(`Creating employee with EMP ID: ${createdEmpId}`);
+    await createTrainingPage.clickAddTraining();
+    await createTrainingPage.fillForm(record);
+    await createTrainingPage.clickAdd();
+    console.log(`Employee created: ${createdEmpId}`);
+    await this.deletepage.deleteEmployee(createdEmpId);
+
 });
 
-Then('the selected employee training record should not be displayed in the list', async function (this: CustomWorld) {
-    await this.deletepage.verifyEmployeeDeleted(deleteData.empId);
-});
+Then("the selected employee training record should not be displayed in the list",{ timeout: 15000 },async function (this: CustomWorld) {
+    await this.deletepage.verifyEmployeeDeleted(createdEmpId);
+    }
+);
